54ea5a17d1a9c890aff076ecc5aadc058385fe33
jpeezzy <jpeezzy@hotmail.com>
2015-10-24 12:09:56 -0700

    Update routes.js

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -5,6 +5,18 @@ angular.module("ionicMeteorSkeleton").config(['$urlRouterProvider', '$stateProvi
         $locationProvider.html5Mode(true);
 
         $stateProvider
+         .state('tasks', {
+            url: '/tasks',
+            templateUrl: 'client/views/task-list.ng.html'
+        })
+        .state('taskForm', {
+            url: '/taskForm',
+            templateUrl: 'client/views/task-form.ng.html'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'client/views/login.ng.html'
+        });
 
 
         $urlRouterProvider.otherwise('tasks');
